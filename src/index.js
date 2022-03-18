68d2667e3f4e9bbbb13af1a0d647de2550f3f1fb
Mohammed Mahgoub <mmahgoub@gmail.com>
2022-03-18 17:44:59 +0200

    challenge 4

--- src/index.js
+++ src/index.js
@@ -3,24 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import logo from './logo.svg';
+import Page from './Page'
 
-const page = (
-  <div>
-    <img src={logo} className="App-logo" alt="" />
-    <h1>Fun Facts About React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands if enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
 
 ReactDOM.render(
-  page,
+  <Page />,
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
